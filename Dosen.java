edaf6e6d75ac7e9304f7cbb0b2ba1b2cedb9985d
ramadhan.afwan <rama.afwan.mutasodirin3@gmail.com>
2018-09-07 15:02:11 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,29 @@
+
+public class Dosen {
+	public String namaDosen;
+	public int nip;
+	
+	public Dosen(String namaDosen, int nip) {
+		super();
+		this.namaDosen = namaDosen;
+		this.nip = nip;
+	}
+
+	public String getNamaDosen() {
+		return namaDosen;
+	}
+
+	public void setNamaDosen(String namaDosen) {
+		this.namaDosen = namaDosen;
+	}
+
+	public int getNip() {
+		return nip;
+	}
+
+	public void setNip(int nip) {
+		this.nip = nip;
+	}
+	
+	
+}
